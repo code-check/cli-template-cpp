e2cc07fb98c2cc2a53532f32ee26cb14d7bd97b9
Takayuki Oda <takayuki@odashi.me>
2016-12-22 12:34:16 +0900

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,8 @@ using namespace std;
 
 int main(int argc, char *argv[])
 {
-  for (int i=1; i<argc; i++) {
+  // start from 1 to ignore script name; argv[0] will be a name of processing file.
+  for (int i = 1; i < argc; i++) {
     cout << argv[i] << endl;
   }
   return 0;
